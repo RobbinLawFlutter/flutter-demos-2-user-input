e59370e8073b771b1fb0588ce644f4ae4fdead31
robbinl <robbinl@telusplanet.net>
2021-01-14 13:48:33 -0700

    a

--- qq-dropdown-menu-themed/lib/main.dart
+++ qq-dropdown-menu-themed/lib/main.dart
@@ -1,87 +1,134 @@
-//ListView Widget of the Week
-//https://www.youtube.com/watch?v=KJpkjHGiI5A&vl=en
-
-//TextEditingController Widget
-//https://api.flutter.dev/flutter/widgets/TextEditingController-class.html
-
-//TextField Widget
-//https://api.flutter.dev/flutter/material/TextField-class.html
-
 import 'package:flutter/material.dart';
 
-// This app does
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-const kTextFieldInputDecoration = InputDecoration(
-  filled: true,
-  fillColor: Colors.white,
-  icon: Icon(
-    Icons.thumb_up,
-    color: Colors.white,
-  ),
-  hintText: 'Enter Your Name',
-  hintStyle: TextStyle(
-    color: Colors.grey,
-  ),
-  border: OutlineInputBorder(
-    borderRadius: BorderRadius.all(
-      Radius.circular(10),
-    ),
-    borderSide: BorderSide.none,
-  ),
-);
-
-class App extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  List<String> listItems = [];
-  final TextEditingController textEditingController = TextEditingController();
-  @override
-  Widget build(BuildContext ctxt) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            title: Text("TextField and ListView.builder Demo"),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  controller: textEditingController,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                  ),
-                  decoration: kTextFieldInputDecoration,
-                  onChanged: (value) {
-                    print(value);
-                  },
-                  onSubmitted: (text) {
-                    listItems.add(text);
-                    textEditingController.clear();
-                    setState(() {});
-                  },
-                ),
-              ),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: listItems.length,
-                      itemBuilder: (BuildContext ctxt, int index) {
-                        return Text(
-                          listItems[index],
-                          style: TextStyle(
-                            fontSize: 30,
-                          ),
-                        );
-                      }))
-            ],
-          )),
+      title: 'Dropdown Menu Themed Demo',
+      debugShowCheckedModeBanner: false,
+      home: DropdownMenuDemo(),
+      theme: _buildShrineTheme(),
     );
   }
 }
+
+class DropdownMenuDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dropdown Menu Themed Demo'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.add),
+                  title: Text('Item 1'),
+                ),
+              ),
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.anchor),
+                  title: Text('Item 2'),
+                ),
+              ),
+              const PopupMenuItem(
+                child: ListTile(
+                  leading: Icon(Icons.article),
+                  title: Text('Item 3'),
+                ),
+              ),
+              const PopupMenuDivider(),
+              const PopupMenuItem(child: Text('Item A')),
+              const PopupMenuItem(child: Text('Item B')),
+            ],
+          ),
+        ],
+      ),
+      body: Center(),
+    );
+  }
+}
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: _shrineColorScheme,
+    accentColor: shrineBrown900,
+    primaryColor: shrinePink100,
+    buttonColor: shrinePink100,
+    scaffoldBackgroundColor: shrineBackgroundWhite,
+    cardColor: shrineBackgroundWhite,
+    textSelectionColor: shrinePink100,
+    errorColor: shrineErrorRed,
+    buttonTheme: const ButtonThemeData(
+      colorScheme: _shrineColorScheme,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    primaryIconTheme: _customIconTheme(base.iconTheme),
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
+    iconTheme: _customIconTheme(base.iconTheme),
+  );
+}
+
+IconThemeData _customIconTheme(IconThemeData original) {
+  return original.copyWith(color: shrineBrown900);
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+          letterSpacing: defaultLetterSpacing,
+        ),
+        button: base.button.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+          letterSpacing: defaultLetterSpacing,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: shrineBrown900,
+        bodyColor: shrineBrown900,
+      );
+}
+
+const ColorScheme _shrineColorScheme = ColorScheme(
+  primary: shrinePink100,
+  primaryVariant: shrineBrown900,
+  secondary: shrinePink50,
+  secondaryVariant: shrineBrown900,
+  surface: shrineSurfaceWhite,
+  background: shrineBackgroundWhite,
+  error: shrineErrorRed,
+  onPrimary: shrineBrown900,
+  onSecondary: shrineBrown900,
+  onSurface: shrineBrown900,
+  onBackground: shrineBrown900,
+  onError: shrineSurfaceWhite,
+  brightness: Brightness.light,
+);
+
+const Color shrinePink50 = Color(0xFFFEEAE6);
+const Color shrinePink100 = Color(0xFFFEDBD0);
+const Color shrinePink300 = Color(0xFFFBB8AC);
+const Color shrinePink400 = Color(0xFFEAA4A4);
+
+const Color shrineBrown900 = Color(0xFF442B2D);
+const Color shrineBrown600 = Color(0xFF7D4F52);
+
+const Color shrineErrorRed = Color(0xFFC5032B);
+
+const Color shrineSurfaceWhite = Color(0xFFFFFBFA);
+const Color shrineBackgroundWhite = Colors.white;
+
+const defaultLetterSpacing = 0.03;
