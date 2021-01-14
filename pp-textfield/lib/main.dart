d1d435239da921da191d28b0e456287d218d6638
robbinl <robbinl@telusplanet.net>
2021-01-14 09:59:02 -0700

    a

--- pp-textfield/lib/main.dart
+++ pp-textfield/lib/main.dart
@@ -1,56 +1,86 @@
+//ListView Widget of the Week
+//https://www.youtube.com/watch?v=KJpkjHGiI5A&vl=en
+
+//TextEditingController Widget
+//https://api.flutter.dev/flutter/widgets/TextEditingController-class.html
+
+//TextField Widget
+//https://api.flutter.dev/flutter/material/TextField-class.html
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MyApp(),
-  );
+// This app does
+void main() => runApp(App());
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter Your Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
+class _AppState extends State<App> {
+  List<String> listItems = [];
+  final TextEditingController eCtrl = TextEditingController();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctxt) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text("Card, ListTile, and Divider Widgets"),
-          backgroundColor: Colors.blue,
-        ),
-        body: SizedBox(
-          height: 250,
-          child: Card(
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text('1625 Main Street',
-                      style: TextStyle(fontWeight: FontWeight.w500)),
-                  subtitle: Text('My City, CA 99984'),
-                  leading: Icon(
-                    Icons.restaurant_menu,
-                    color: Colors.blue[500],
-                  ),
-                ),
-                Divider(),
-                ListTile(
-                  title: Text('(408) 555-1212',
-                      style: TextStyle(fontWeight: FontWeight.w500)),
-                  leading: Icon(
-                    Icons.contact_phone,
-                    color: Colors.blue[500],
-                  ),
-                ),
-                ListTile(
-                  title: Text('costa@example.com'),
-                  leading: Icon(
-                    Icons.contact_mail,
-                    color: Colors.blue[500],
-                  ),
-                ),
-              ],
-            ),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("TextField and ListView.builder Demo"),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  controller: eCtrl,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  onSubmitted: (text) {
+                    listItems.add(text);
+                    eCtrl.clear();
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: listItems.length,
+                      itemBuilder: (BuildContext ctxt, int index) {
+                        return Text(
+                          listItems[index],
+                          style: TextStyle(
+                            fontSize: 30,
+                          ),
+                        );
+                      }))
+            ],
+          )),
     );
   }
 }
