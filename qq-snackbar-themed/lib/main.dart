6de7a943b3cd5b3d654763ebf934226a6d5a3fa5
robbinl <robbinl@telusplanet.net>
2021-01-24 15:36:59 -0700

    a

---
+++ qq-snackbar-themed/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:robbinlaw/themes/shrinetheme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dropdown Menu Themed Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyPage(),
+      theme: buildShrineTheme(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top AppBar'),
+        actions: [SnackbarButton()],
+      ),
+      body: Center(),
+    );
+  }
+}
+
+class SnackbarButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        final snackBar = SnackBar(
+          behavior: SnackBarBehavior.floating,
+          content: Text('Text label'),
+          action: SnackBarAction(
+            label: 'Action',
+            onPressed: () {
+              print('hey you clicked on the snackbar Action');
+            },
+          ),
+        );
+
+        // Find the Scaffold in the widget tree and use
+        // it to show a SnackBar.
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Text('Show SnackBar'),
+    );
+  }
+}
