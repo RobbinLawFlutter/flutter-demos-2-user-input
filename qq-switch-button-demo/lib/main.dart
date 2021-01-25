5c9432c13c3cbed69f86862069bff17da4c9136f
robbinl <robbinl@telusplanet.net>
2021-01-24 17:23:07 -0700

    a

---
+++ qq-switch-button-demo/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:robbinlaw/themes/shrinetheme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Switch Themed Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      theme: buildShrineTheme(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<bool> _values = [true, false, true, false, false];
+
+  Widget build(BuildContext context) {
+    final int count = 4;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Switch Themed Demo'),
+      ),
+      body: Column(
+        children: <Widget>[
+          for (int i = 0; i <= count; i++)
+            ListTile(
+              title: Text(
+                'Switch $i',
+              ),
+              leading: Switch(
+                value: _values[i],
+                onChanged: i == count
+                    ? null
+                    : (bool value) {
+                        _values[i] = value;
+                        print(_values);
+                        setState(() {});
+                      },
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
