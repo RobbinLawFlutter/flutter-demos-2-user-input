4f5b94af1f3a7cf25636287db94dfd08e72fdfb4
robbinl <robbinl@telusplanet.net>
2021-01-29 10:19:17 -0700

    a

--- zz-app2/lib/first-page.dart
+++ zz-app2/lib/first-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:robbinlaw/themes/shrinetheme.dart';
 
 class MyFirstPage extends StatefulWidget {
   @override
@@ -7,33 +8,39 @@ class MyFirstPage extends StatefulWidget {
 
 class _MyFirstPageState extends State<MyFirstPage> {
   bool _enabled = false;
-  String _msg1 = 'Disabled';
-  //String _msg1 = '';
+  int _timesClicked = 0;
+  String _msg1 = '';
+  String _msg2 = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Enable Button Demo'),
+        title: Text('App2'),
       ),
-      body: Center(
+      body: Container(
         child: Column(
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Enable Button'),
+                Text('Enable Buttons'),
                 Switch(
                     value: _enabled,
                     onChanged: (bool onChangedValue) {
+                      _enabled = onChangedValue;
                       setState(() {
-                        _enabled = onChangedValue;
                         if (_enabled) {
-                          _msg1 = 'Enabled';
+                          if (_timesClicked == 0) {
+                            _msg1 = 'Click Me';
+                          } else {
+                            _msg1 = 'Clicked $_timesClicked';
+                          }
+                          _msg2 = 'Reset';
                           print('_enabled is true');
                         } else {
-                          _msg1 = 'Disabled';
-                          //_msg1 = '';
+                          _msg1 = '';
+                          _msg2 = '';
                           print('_enabled is false');
                         }
                       });
@@ -43,25 +50,54 @@ class _MyFirstPageState extends State<MyFirstPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ElevatedButton(
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  elevation: 8,
+                  color: shrinePink100,
+                  padding: EdgeInsets.all(15.0),
                   onPressed: _enabled
                       ? () {
-                          print('Button Pressed');
+                          setState(() {
+                            _timesClicked++;
+                            _msg1 = 'Clicked $_timesClicked';
+                            print('clicked $_timesClicked');
+                          });
                         }
                       : null,
                   child: Text(_msg1),
                 ),
+                SizedBox(
+                  width: 10,
+                ),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  elevation: 8,
+                  color: shrinePink100,
+                  padding: EdgeInsets.all(15.0),
+                  onPressed: _enabled
+                      ? () {
+                          setState(() {
+                            _timesClicked = 0;
+                            _msg1 = 'Click Me';
+                            print('Reset Pressed');
+                          });
+                        }
+                      : null,
+                  child: Text(_msg2),
+                ),
               ],
             ),
             SizedBox(
-              height: 20,
+              height: 40,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text('Hey This Is Fun'),
               ],
-            )
+            ),
           ],
         ),
       ),
--- zz-app2/lib/main.dart
+++ zz-app2/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:robbinlaw/themes/shrinetheme.dart';
 import 'package:robbinlaw/first-page.dart';
-import 'package:robbinlaw/second-page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button Enable Demo',
+      title: 'App2',
       debugShowCheckedModeBanner: false,
       home: MyFirstPage(),
       //home: MySecondPage(),
