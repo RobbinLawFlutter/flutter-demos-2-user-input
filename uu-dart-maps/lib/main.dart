4b7b7858e439d14862dd2eac2ef49f6fcada082f
robbinl <robbinl@telusplanet.net>
2021-02-09 17:01:10 -0700

    a

--- uu-dart-maps/lib/main.dart
+++ uu-dart-maps/lib/main.dart
@@ -1,161 +1,81 @@
-// This app shows why it is good to modularize, abstract, and
-// encapsulate code.
-// four pillars of OOP
-// Abstraction by using classes and instanciating objects from the class template.
-// Encapsulation "_" make members private to the class.
-// Inheritance "extends" the base class template.
-// Polymorphism "@override" changes methods of the base class.
+//https://dart.dev/guides/language/language-tour#maps
 
-//Challenge: Make the scoreKeeper list part of the quizMaster class.
+//Maps are generic and both the key and value can be any type.
+//
+Map<String, int> myAgeBook = {
+  'Rob': 60,
+  'Linda': 70,
+  'Bob': 40,
+  'James': 30,
+};
+//We could use var here as well
+//var hawaiianBeaches = {};
+Map<String, List<String>> hawaiianBeaches = {
+  'Oahu': ['Waikiki', 'Kailua', 'Waimanalo'],
+  'Big Island': ['Wailea Bay', 'Pololu Beach'],
+  'Kauai': ['Hanalei', 'Poipu']
+};
 
-import 'package:flutter/material.dart';
-import 'package:robbinlaw/controllers/quizMaster.dart';
+//Maps can be built from a constructor.
+//Maps are parameterized types; you can specify what
+//types the key and value should be explicitly.
+var nobleGases = Map<int, String>();
 
-QuizMaster quizMaster = QuizMaster();
+void main() {
+  //With this syntax the key is 'Bob' and the value is returned.
+  print(myAgeBook['Bob']);
+  //If key is not in the map a NULL is returned.
+  print(myAgeBook['Jim']);
+  //Adding a new key:value to the map.
+  myAgeBook['Tim'] = 20;
+  //Iterate thru the map.
+  myAgeBook.forEach((key, value) {
+    print('key is $key, value is $value');
+  });
+  //Common Map properties: length, keys, values, isEmpty, isNotEmpty
+  print('myAgeBook Length is: ${myAgeBook.length}');
+  print('myAgeBook Keys are: ${myAgeBook.keys}');
+  print('myAgeBook Values are: ${myAgeBook.values}');
+  print('myAgeBook isEmpty is: ${myAgeBook.isEmpty}');
+  print('myAgeBook isNotEmpty is: ${myAgeBook.isNotEmpty}');
+  //Common Map methods: remove, from, of,
+  Map map = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'};
+  map.remove(2);
+  print(map);
 
-void main() => runApp(MyApp());
+  map.removeWhere((k, v) => v.startsWith('f'));
+  print(map);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //theme: buildShrineTheme(),
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: MyFirstPage(),
-          ),
-        ),
-      ),
-    );
-  }
-}
+  map.clear();
+  print(map);
 
-class MyFirstPage extends StatefulWidget {
-  @override
-  _MyFirstPageState createState() => _MyFirstPageState();
-}
+  var map2 = Map.from(myAgeBook);
+  print(map2);
 
-class _MyFirstPageState extends State<MyFirstPage> {
-  List<Icon> scoreKeeper = [];
+  var map3 = Map.of(myAgeBook);
+  print(map3);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                quizMaster.getQuestionText(),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                checkAnswer(true);
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                checkAnswer(false);
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.amber,
-              child: Text(
-                'Start Over',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                startOver();
-              },
-            ),
-          ),
-        ),
-        Row(
-          children: scoreKeeper,
-        ),
-      ],
-    );
-  }
+  print(hawaiianBeaches.keys);
+  print(hawaiianBeaches.values);
+  print(hawaiianBeaches['Big Island']);
+  print(hawaiianBeaches['Oahu'][1]);
 
-  void checkAnswer(bool userAnswer) {
-    setState(() {
-      if (quizMaster.isNotFinished() == true) {
-        bool correctAnswer = quizMaster.getQuestionAnswer();
-        if (userAnswer == correctAnswer) {
-          print('got it right');
-          scoreKeeper.add(
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-          );
-        } else {
-          print('got it wrong');
-          scoreKeeper.add(
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          );
-        }
-        quizMaster.nextQuestion();
-      }
-    });
-  }
+  //You can also allow for the key and value types to change
+  //on the fly programatically.
+  var planets = Map();
 
-  void startOver() {
-    setState(() {
-      quizMaster.reset();
-      scoreKeeper = [];
-    });
-  }
+  planets[1] = 'Pluto';
+  planets[4] = 'Jupiter';
+  planets['this key is a string'] = 20;
+  planets.forEach((key, value) {
+    print(planets);
+  });
+
+  var map4 = {1: 'one', 2: 'two', 3: 'three'};
+
+  var transformedMap = map4.map((k, v) {
+    return MapEntry('($k)', v.toUpperCase());
+  });
+
+  print(transformedMap);
 }
